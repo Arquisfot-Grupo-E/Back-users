c3677c9c0306fc20b9cdd1243ea3127962acffb1
Valentina Chicuasuque Rodriguez <chicuasuquevalentina@gmail.com>
2025-09-27 14:40:01 -0500

    preferences
    Cuando recupere la informacion de un usaurio saber si ya hizo lo de las preferencias

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -22,6 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'description',
             'is_active',
+            'has_selected_preferences',
             'is_staff',
         ]
 
