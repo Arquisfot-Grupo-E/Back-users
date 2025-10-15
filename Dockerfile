2bb249c836a1d806a39e0dba1ff81850872ce956
Manuel Quintana <manuel.quintana@nuvu.cc>
2025-10-14 21:13:00 -0500

    fix: settings dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@
 FROM python:3.12-slim
 
 # Establecer variables de entorno para Python
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
 
 # Establecer el directorio de trabajo
 WORKDIR /app
--- back_users/settings.py
+++ back_users/settings.py
@@ -93,6 +93,8 @@ from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'SIGNING_KEY': SECRET_KEY,
+    'ALGORITHM': 'HS256',
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
